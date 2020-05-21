dff09b3a3dc129f9a934e49f74807f128f967f61
Ankur <vasavada.ankur@gmail.com>
2020-05-21 18:57:38 -0400

    fixed small issue

--- Express-Lab-2.sql
+++ Express-Lab-2.sql
@@ -1,5 +1,5 @@
-INSERT INTO shopping_cart (id,  product, price, quantity)
-VALUES (10, "Bitters Bottle", 23, 124),
-VALUES (11, "Dasher Top", 16, 25),
-VALUES (12, "Lewis Bar", 22, 10),
-VALUES (13, "Jigger", 9, 16);
+INSERT INTO shopping_cart (product_id,  product, price, quantity)
+VALUES ('10', 'Bitters Bottle', '23', '124'),
+('11', 'Dasher Top', '16', '25'),
+('12', 'Lewis Bar', '22', '10'),
+('13', 'Jigger', '9', '16');
